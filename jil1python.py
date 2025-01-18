3d139b855f95d7c7031f1abd74b663dcaae05d65
Jil Patel <jilpatel0310@gmail.com>
2025-01-18 09:42:22 +0530

    Add files via upload

---
+++ jil1python.py
@@ -0,0 +1,39 @@
+# 1. Addition of two numbers
+a=3
+b=4
+c=a+b
+print(c)
+# 2. Subtration of two numbers
+d=b-a
+print(d)
+# 3. Multiplication of two numbers
+e=a*b
+print(e)
+# 4. Division of two numbers
+c=a/b
+print(c)
+# 5. Convert hour into minutes
+a=3
+min=60*a
+print(min)
+# 6. Convert minutes into hour
+min=180
+hour=min//60
+print(hour)
+# 7. Convert dollar into rupees
+dollar=120
+rupees=dollar*48
+print(rupees)
+# 8. Convert rupees into dollar
+rupees=300
+dollar=rupees/48
+print(dollar)
+# 9. Convert dollar into pounds
+dollar=5603
+rupees=dollar*48
+pound=rupees/70
+print(pound)
+# 10. Convert gram into kilogram
+gram=3
+kg=1000*gram
+print(kg)
